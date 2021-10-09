d323c100d851c46db3a4add31127b844f06fc621
well50504 <rex50504@gmail.com>
2021-10-09 14:03:20 +0800

    image update

--- components/Image/index.js
+++ components/Image/index.js
@@ -1,7 +1,7 @@
 export const funcImg = (url) => {
   let img = '';
   if (url != '') {
-    img = `/cgu.github.io${url}`;
+    img = `/cgu.github.io-master${url}`;
     //img = `${url}`;
     return img;
   }
